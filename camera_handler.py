b8cb547b19ad0ee66b21f98e2ab86bec609d1798
GabrielHugo <hugogabriel1412@hotmail.com>
2026-03-04 14:50:45 +0100

    Mise en place de l'architecture 00 et configuration du flux caméra

---
+++ camera_handler.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class SkyCamera :
+
+    def __init__(self) :
+        capture = os.getenv("CAMERA_URL")
+
+        self.camera = cv2.VideoCapture(capture)
+
+    def get_frame(self) :
+        ret, frame = self.camera.read()
+        return ret, frame
+
+    def release_camera(self) :
+        self.camera.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__" :
+    camera = SkyCamera()
+
+    while True :
+        success, img = camera.get_frame()
+
+        if not success :
+            print("Erreur de lecture")
+            break
+
+        cv2.imshow( "Capture",img)
+
+        if cv2.waitKey(1) == ord("q") :
+            break
+
+    camera.release_camera()
---
+++ main.py
@@ -0,0 +1,3 @@
+from camera_handler import SkyCamera
+
+SkyCamera()
